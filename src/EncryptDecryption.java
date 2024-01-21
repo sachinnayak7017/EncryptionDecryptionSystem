bac44215caf1b1ba8d910b355b090902bf215c98
sachinnayak7017 <sachinnayak7017@gmail.com>
2024-01-21 10:41:58 +0530

    name change

--- src/EncryptDecryption.java
+++ src/EncryptDecryption.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.Scanner;
 
-public class EncyptDecyption {
+public class EncryptDecryption {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
